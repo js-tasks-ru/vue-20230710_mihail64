e357406e279c05528a8739bc9e3ea3440e7abdd6
m4ex <4exmail@gmail.com>
2023-07-15 18:31:49 +0300

    01-01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,15 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  template: `
+    <button type="button" @click="counter++">{{ counter }}</button>`,
+});
+const app = createApp(App);
+app.mount('#app');
