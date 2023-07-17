a7cf42386404cd43bd3633fbc2d5a07eb93b3902
m4ex <4exmail@gmail.com>
2023-07-18 02:39:37 +0300

    02-04-MeetupInfo

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -2,20 +2,37 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  props: {
+    organizer: { type: String, required: true },
+    place: { type: String, required: true },
+    date: { type: Number, required: true },
+  },
+  computed: {
+    isoDate() {
+      return new Date(this.$props.date).toISOString().split('T')[0];
+    },
 
+    localDate() {
+      return new Date(this.$props.date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+  },
   template: `
     <ul class="meetup-info">
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
-      </li>
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
-      </li>
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
-      </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+      {{ organizer }}
+    </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+      {{ place }}
+    </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+      <time :datetime="isoDate">{{ localDate }}</time>
+    </li>
     </ul>`,
 });
