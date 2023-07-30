5c6205b3cdb6d98dc4d0c09e0afcf2f5a07ffbb4
m4ex <4exmail@gmail.com>
2023-07-31 00:51:34 +0300

    04-vue-cli/02-TheToaster

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <UiIcon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <UiIcon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+    <UIToast v-for="toast in toasts" :key="toast.id" :message="toast.message" :type="toast.type" />
   </div>
 </template>
 
 <script>
-import UiIcon from './UiIcon.vue';
+import UIToast from './UIToast.vue';
 
 export default {
+  expose: ['success', 'error'],
   name: 'TheToaster',
-
-  components: { UiIcon },
+  components: { UIToast },
+  data() {
+    return {
+      toasts: [],
+      counter: 0,
+    };
+  },
+  methods: {
+    success(message) {
+      this.newToast(message, 'success', 5000);
+    },
+    error(message) {
+      this.newToast(message, 'error', 5000);
+    },
+    newToast(message, type, timeout) {
+      this.toasts.push({ id: this.counter, type: type, message: message });
+      setTimeout(
+        (id) => {
+          this.toasts = this.toasts.filter(function (obj) {
+            return obj.id !== id;
+          });
+        },
+        timeout,
+        this.counter,
+      );
+      this.counter++;
+    },
+  },
 };
 </script>
 
@@ -39,35 +58,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
