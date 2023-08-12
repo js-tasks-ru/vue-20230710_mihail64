eed0c0e060b0481824fb57253273db9cbf4a5145
m4ex <4exmail@gmail.com>
2023-08-13 00:49:28 +0300

    06-wrappers/02-UiButton

--- 06-wrappers/02-UiButton/components/UiButton.vue
+++ 06-wrappers/02-UiButton/components/UiButton.vue
@@ -1,10 +1,31 @@
 <template>
-  <button class="button button_secondary button_block">BUTTON</button>
+  <component
+    :is="$props.tag"
+    class="button"
+    :type="tag === 'button' ? 'button' : null"
+    :class="{ button_block: $props.block, [`button_${$props.variant}`]: true }"
+  >
+    <slot />
+  </component>
 </template>
 
 <script>
 export default {
   name: 'UiButton',
+  props: {
+    tag: {
+      type: [String, Object],
+      default: 'button',
+    },
+    variant: {
+      type: String,
+      default: 'secondary',
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
 
