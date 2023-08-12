d4cf70d022b2c5482a31172cc7fc9e1250c4b6d5
m4ex <4exmail@gmail.com>
2023-08-12 21:49:43 +0300

    05-vue-router/03-ScrollBehavior

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -2,7 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
-
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+      return;
+    } else if (to.hash) {
+      return {
+        el: to.hash,
+      };
+    } else {
+      return { top: 0, left: 0 };
+    }
+  },
   routes: [
     {
       path: '/',
