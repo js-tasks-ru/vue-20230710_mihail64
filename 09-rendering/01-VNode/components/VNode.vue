4c626b0a23720808049f0f017e7eb5d550e0c752
m4ex <4exmail@gmail.com>
2023-08-20 01:06:32 +0300

    09-rendering/01-VNode

--- 09-rendering/01-VNode/components/VNode.vue
+++ 09-rendering/01-VNode/components/VNode.vue
@@ -11,5 +11,8 @@ export default {
       validator: (value) => (Array.isArray(value) ? value.every((item) => isVNode(item)) : isVNode(value)),
     },
   },
+  render() {
+    return Array.isArray(this.vnode) ? [...this.vnode] : this.vnode;
+  },
 };
 </script>
