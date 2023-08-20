69826f2558cd78b8e7e33afd3a9514a8b7d33365
m4ex <4exmail@gmail.com>
2023-08-21 01:03:29 +0300

    09-rendering/02-TemplateRenderer

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,24 +1,39 @@
 <script>
-// import { compile } from 'vue';
+import { compile } from 'vue/dist/vue.cjs';
 
 export default {
   name: 'TemplateRenderer',
-
   props: {
     template: {
       type: String,
       required: true,
     },
-
     bindings: {
       type: Object,
       default: () => ({}),
     },
-
     components: {
       type: [Object, Array],
       default: () => [],
     },
+  },
+  methods: {
+    setComponents() {
+      for (const componentName in this.components) {
+        this.$options.components[componentName] = this.components[componentName];
+      }
+    },
+  },
+  components: {},
+  created() {
+    this.setComponents();
+  },
+  beforeUpdate() {
+    this.setComponents();
+  },
+  render() {
+    const renderFunction = compile(this.template);
+    return renderFunction({ bindings: this.bindings });
   },
 };
 </script>
