744948237e60968a3dbcf2465df39b9ffb274b45
m4ex <4exmail@gmail.com>
2023-08-22 12:24:46 +0300

    10-slots/01-UiPaginationView

--- 10-slots/01-UiPaginationView/components/UiPaginationView.vue
+++ 10-slots/01-UiPaginationView/components/UiPaginationView.vue
@@ -1,29 +1,35 @@
 <template>
   <div class="pagination-container">
     <!-- Контент страницы -->
+    <template v-for="item in paginatedItems" :key="item.id">
+      <slot :item="item"></slot>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'UiPaginationView',
-
   props: {
     page: {
       type: Number,
       required: true,
       validator: (value) => value > 0,
     },
-
     perPage: {
       type: Number,
       required: true,
       validator: (value) => value > 0,
     },
-
     items: {
       type: Array,
       required: true,
+    },
+  },
+  computed: {
+    paginatedItems() {
+      const startIndex = (this.page - 1) * this.perPage;
+      return this.items.slice(startIndex, startIndex + this.perPage);
     },
   },
 };
