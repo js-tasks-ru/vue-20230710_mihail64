0c2f1f3da19219e9e3cfb1ad84e969d9213571df
m4ex <4exmail@gmail.com>
2023-08-22 13:44:26 +0300

    10-slots/02-PromiseWrapper

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,13 @@
 <template>
-  <!-- -->
+  <template v-if="status === 'pending'">
+    <slot name="pending" />
+  </template>
+  <template v-if="status === 'rejected'">
+    <slot name="rejected" :error="error" />
+  </template>
+  <template v-if="status === 'fulfilled'">
+    <slot name="fulfilled" :result="result" />
+  </template>
 </template>
 
 <script>
@@ -10,6 +18,35 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+  data() {
+    return {
+      status: 'pending',
+      error: null,
+      result: null,
+    };
+  },
+  methods: {
+    async resolvePromise(promise) {
+      try {
+        this.result = await promise;
+        this.status = 'fulfilled';
+      } catch (e) {
+        this.status = 'rejected';
+        this.error = e;
+      }
+    },
+  },
+  watch: {
+    promise: {
+      handler(newPromise) {
+        this.status = 'pending';
+        this.error = null;
+        this.result = null;
+        this.resolvePromise(newPromise);
+      },
+      immediate: true,
     },
   },
 };
